11bd1ec10236f907725f72fb6687da2cfafcde1f
oran2527 <orlago250183@gmail.com>
2020-03-12 13:29:16 +0000

    point 6 add NULL

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,6 +8,8 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
 	if (tree != NULL || func != NULL)
 	{
 		func(tree->n);
