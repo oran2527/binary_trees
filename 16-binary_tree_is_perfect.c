de5baa3d81acbab3d43cbc916251ffa472d7cd78
oran2527 <orlago250183@gmail.com>
2020-03-13 21:20:48 +0000

    point 17 sibling

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - check if tree is perfect
+ * @tree: node
+ * Return: 1 if full if not 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+
+}
